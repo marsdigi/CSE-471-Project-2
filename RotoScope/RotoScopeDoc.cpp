29241cb18134839e82989931a0e7370d9c85fda9
agustbrandinger <agust.brandinger@gmail.com>
2023-11-28 19:43:22 -0500

    Set Profile Name

--- RotoScope/RotoScopeDoc.cpp
+++ RotoScope/RotoScopeDoc.cpp
@@ -69,6 +69,9 @@ CRotoScopeDoc::CRotoScopeDoc()
 	m_bird.LoadFile(L"birdp.png");
 
 	//OnEditSetvariables();
+
+	// Set Profile
+	m_moviemake.SetProfileName(L"profile720p.prx");
 }
 
 //! Destructor
